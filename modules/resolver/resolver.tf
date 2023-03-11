b2cc00ae4baadd38feb2e974b642428d41c91f35
panchalnimesh <npanchal@sph.com.sg>
2023-03-11 11:39:32 +0800

    fix rule

--- modules/resolver/resolver.tf
+++ modules/resolver/resolver.tf
@@ -39,14 +39,14 @@ resource "aws_security_group" "this" {
 
   ingress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["10.0.0.0/8"]
   }
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
