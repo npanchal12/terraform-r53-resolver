eaa158a25559d8d30401ff02b9d0be3ddcdfe749
panchalnimesh <npanchal@sph.com.sg>
2023-03-13 17:47:42 +0800

    create resource

--- env/prd/resolver/main.tf
+++ env/prd/resolver/main.tf
@@ -1,12 +1,12 @@
-# module "sph_corp_resolver" {
-#   source = "../../../modules/resolver"
+module "sph_corp_resolver" {
+  source = "../../../modules/resolver"
 
-#   ip_address = [
-#     {
-#       subnet_id = "subnet-0e0fa656e9b3e3287"
-#     },
-#     {
-#       subnet_id = "subnet-0b6450fc97b23be2f"
-#     },
-#   ]
-# }
+  ip_address = [
+    {
+      subnet_id = "subnet-0e0fa656e9b3e3287"
+    },
+    {
+      subnet_id = "subnet-0b6450fc97b23be2f"
+    },
+  ]
+}
